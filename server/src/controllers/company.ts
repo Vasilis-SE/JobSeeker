975ceccf2b646833604229512da6492e34186183
Vasilis CodeX <vasilis.tris@gmail.com>
2022-03-15 23:54:29 +0200

    connect company api with the rest of the application

--- server/src/controllers/company.ts
+++ server/src/controllers/company.ts
@@ -1,36 +1,44 @@
 import { NextFunction } from 'express';
+import { ICompanyProperties } from '../interfaces/company';
 import { InjectedRequest, InjectedResponse } from '../interfaces/express';
 import { IFailedResponse, ISuccessfulResponse } from '../interfaces/response';
 import { IUserProperties } from '../interfaces/user';
+import CompanyService from '../services/company';
 
 /**
  * Controller class for 'company' domain. All those class functions are connected
  * directly with one or more routes.
  */
 export default class CompanyController {
-    // private _service: UserService;
+    private _service: CompanyService;
 
     constructor() {
-        // this._service = new UserService();
+        this._service = new CompanyService();
     }
 
     async createCompany(req: InjectedRequest, res: InjectedResponse, next: NextFunction): Promise<void> {
-        const params: IUserProperties = { id: req.user.id };
-        const response: ISuccessfulResponse | IFailedResponse = await this._service.getUsers(params, {});
+        const payload: ICompanyProperties = req.body;
+        const user: IUserProperties = req.user;
+
+        const response: ISuccessfulResponse | IFailedResponse = await this._service.createCompany(payload, user);
         res.response = response;
         next();
     }
 
     async updateCompany(req: InjectedRequest, res: InjectedResponse, next: NextFunction): Promise<void> {
-        const params: IUserProperties = { id: req.user.id };
-        const response: ISuccessfulResponse | IFailedResponse = await this._service.getUsers(params, {});
+        const payload: ICompanyProperties = req.body;
+        const user: IUserProperties = req.user;
+
+        const response: ISuccessfulResponse | IFailedResponse = await this._service.updateCompany(payload, user);
         res.response = response;
         next();
     }
 
     async deleteCompany(req: InjectedRequest, res: InjectedResponse, next: NextFunction): Promise<void> {
-        const params: IUserProperties = { id: req.user.id };
-        const response: ISuccessfulResponse | IFailedResponse = await this._service.getUsers(params, {});
+        const params: ICompanyProperties = req.params;
+        const user: IUserProperties = req.user;
+
+        const response: ISuccessfulResponse | IFailedResponse = await this._service.deleteCompany(params, user);
         res.response = response;
         next();
     }
--- server/src/routes/company.ts
+++ server/src/routes/company.ts
@@ -4,29 +4,29 @@ import CompanyController from '../controllers/company';
 import { InjectedRequest, InjectedResponse } from '../interfaces/express';
 import Security from '../security/security';
 
-const userRoutes = Router();
+const companyRoutes = Router();
 const _security = new Security();
 const _baseController = new BaseController();
 const _controller = new CompanyController();
 
-userRoutes.post(
+companyRoutes.post(
     '/',
     (req: InjectedRequest, res: InjectedResponse, next: NextFunction) => _security.authenticate(req, res, next),
     (req: InjectedRequest, res: InjectedResponse, next: NextFunction) => _controller.createCompany(req, res, next),
     (req: InjectedRequest, res: InjectedResponse, next: NextFunction) => _baseController.send(req, res, next),
 );
 
-userRoutes.patch(
+companyRoutes.patch(
     '/',
     (req: InjectedRequest, res: InjectedResponse, next: NextFunction) => _security.authenticate(req, res, next),
     (req: InjectedRequest, res: InjectedResponse, next: NextFunction) => _controller.updateCompany(req, res, next),
     (req: InjectedRequest, res: InjectedResponse, next: NextFunction) => _baseController.send(req, res, next),
 );
 
-userRoutes.delete(
-    '/',
+companyRoutes.delete(
+    '/:id([0-9]+)',
     (req: InjectedRequest, res: InjectedResponse, next: NextFunction) => _security.authenticate(req, res, next),
     (req: InjectedRequest, res: InjectedResponse, next: NextFunction) => _baseController.send(req, res, next),
 );
 
-export default userRoutes;
+export default companyRoutes;
