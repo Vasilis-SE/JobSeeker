0ea869238e36e98ef449e586278a84886566b8e7
Vasilis CodeX <vasilis.tris@gmail.com>
2022-03-16 20:00:11 +0200

    finished with job's model functionalities

--- server/src/models/job.ts
+++ server/src/models/job.ts
@@ -1,6 +1,8 @@
-import { IJob, IJobProperties } from "../interfaces/job";
+import PostgreSQL from "../connections/postgres";
+import ObjectHandler from "../helpers/objectHandler";
+import { IJob, IJobFilters, IJobProperties, IListOfJobs } from "../interfaces/job";
 
-export default class CompanyModel implements IJob {
+export default class JobModel implements IJob {
     id!: number;
     companyid!: number;
     title!: string;
@@ -21,17 +23,78 @@ export default class CompanyModel implements IJob {
         this.setDeletedAt(job.deleted_at ? job.deleted_at : 0)
     }
 
-    // TODO: Get jobs
-    // TODO: Create a new job
-    // TODO: Update a current job
+    // OK: Get jobs
+    // OK: Create a new job
+    // OK: Update a current job
     // TODO: Soft delete a job
 
+    async getJobs(filters: IJobFilters = {}): Promise<IListOfJobs | boolean> {
+        try {
+            let results: IListOfJobs = [];
+            const resource = ObjectHandler.getResource(this);
+            const wherePart = ObjectHandler.objectToSQLParams(resource, ' AND ');
 
+            const query = await PostgreSQL.client.query(`SELECT 
+                ${filters.fields ? filters.fields.join(', ') : '*'}
+                FROM jobs 
+                ${wherePart ? `WHERE ${wherePart}` : ''}
+                ${'orderby' in filters ? `ORDER BY ${filters.orderby}` : ''}
+                ${'limit' in filters ? `LIMIT ${filters.limit}` : ''}`);
+            if (query.rowCount === 0) throw Error();
 
+            results = query.rows;
+            return results;
+        } catch (error) {
+            return false;
+        }
+    }
 
+    async createJob(): Promise<boolean> {
+        try {
+            const queryStr = `INSERT INTO jobs (companyid, title, description, created_at) 
+                VALUES ($1, $2, $3, $4) 
+                RETURNING id`;
+            const query = await PostgreSQL.client.query(queryStr, 
+                [this.getCompanyId(), this.getTitle(), this.getDescription(), this.getDeletedAt()]);
 
+            if (query.rowCount === 0) throw Error();
 
+            // Set the newly created id
+            this.setId(query.rows[0].id);
+            return true;
+        } catch (error) {
+            return false;
+        }
+    }
 
+    async updateJob(): Promise<boolean> {
+        try {
+            const queryStr = `UPDATE jobs SET
+                companyid = $1, title = $2, description = $3
+                WHERE id = $4`;
+            const query = await PostgreSQL.client.query(queryStr, 
+                [this.getCompanyId(), this.getTitle(), this.getDescription(), this.getId()]);
+                
+            if (query.rowCount === 0) throw Error();
+            return true;
+        } catch (error) {
+            return false;
+        }
+    }
+
+    async softRemoveJob(): Promise<boolean> {
+        try {
+            const queryStr = `UPDATE jobs SET 
+                deleted_at = $1
+                WHERE id = $2`;
+            const query = await PostgreSQL.client.query(queryStr, 
+                [this.getDeletedAt(), this.getId()]);
+            if (query.rowCount === 0) throw Error();
+            return true;
+        } catch (error) {
+            return false;
+        }
+    }
 
     // Getters - Setters
     getId(): number {
